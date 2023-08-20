19fd7a61addb3da7c86339d63b4dd2cbb54d5150
Aamir Shahzad <Aamir@eSite.pk>
2023-08-20 16:33:02 +0500

    Class 4

---
+++ Class 4/0.2.php
@@ -0,0 +1,16 @@
+<?php
+// mysqli_query(
+// 	$connection,
+// 	'INSERT INTO users (name, email, password) VALUES ("Aamir Shahzad", "Aamir@eSite.pk", "Hmmm, Why I told you?")'
+// );
+
+// $result = mysqli_query(
+// 	$connection,
+// 	'INSERT INTO users (name, email, password) VALUES ("'.$_GET['name'].'", "'.$_GET['email'].'", "'.$_GET['password'].'")'
+// );
+
+$name = 'Aamir Shahzad';
+$email = 'Aamir@eSite.pk';
+$password = 'Hmmm, Why I told you?';
+$sql = "INSERT INTO users (name, email, password) VALUES (\"$name\", \"$email\", \"$password\")";
+echo $sql;
